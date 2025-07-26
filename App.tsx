ccefdd5956662652db43b1a60496698771ad0665
Michael Farquhar <michaelfarquhar4@gmail.com>
2025-07-26 13:58:47 -0400

    Add Orbitron font

--- App.tsx
+++ App.tsx
@@ -1,10 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  useFonts,
+  Orbitron_400Regular,
+  Orbitron_500Medium,
+  Orbitron_700Bold,
+  Orbitron_900Black,
+} from "@expo-google-fonts/orbitron";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  let [fontsLoaded, fontError] = useFonts({
+    Orbitron_400Regular,
+    Orbitron_500Medium,
+    Orbitron_700Bold,
+    Orbitron_900Black,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ fontFamily: "Orbitron_700Bold", fontSize: 30 }}>Yu-Gi-Oh! Life Points</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +41,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
